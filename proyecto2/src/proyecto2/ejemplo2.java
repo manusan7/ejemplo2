46341ae046480c59ed2e37b0da296196217e3063
alumno <alumno@alumno-Lenovo-V14-G5-IRL>
2026-02-17 16:41:23 +0100

    commit y push rama1

--- proyecto2/src/proyecto2/ejemplo2.java
+++ proyecto2/src/proyecto2/ejemplo2.java
@@ -4,6 +4,10 @@ public class ejemplo2 {
 	
 	public static void main(String[] args) {
 				System.out.println("Hola buenas tardes");
+				System.out.println("Hola buenas ");
+				
+				System.out.println("Hola");
+				
 	}
 
 }
