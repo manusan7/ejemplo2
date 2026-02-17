95b6558af7ba6bf071e74f1daa4964d22dbb129a
alumno <alumno@alumno-Lenovo-V14-G5-IRL>
2026-02-17 17:10:40 +0100

    cambiamos a un programa de apache

--- proyecto2/src/proyecto2/ejemplo2.java
+++ proyecto2/src/proyecto2/ejemplo2.java
@@ -1,15 +1,39 @@
 package proyecto2;
-
+import java.io.*;
+import java.util.Scanner;
 public class ejemplo2 {
 	
 	public static void main(String[] args) {
-				System.out.println("Hola buenas tardes");
-				System.out.println("Hola buenas ");
-				
-				System.out.println("Hola");
-				
-				System.out.println("Adios");
-				System.out.println("Adios amigo");
+		FileReader archivoIn;
+        BufferedReader bufferIn;
+        Scanner sc;
+        String cadenaLeida;
+        double sumaLinea = 0;
+        int numeroLinea = 0;
+        double media;
+        
+        try {
+            archivoIn = new FileReader ("entrada1.txt");
+            bufferIn = new BufferedReader (archivoIn);
+            cadenaLeida = bufferIn.readLine();
+            
+            while (cadenaLeida != null) {
+                if (!cadenaLeida.isBlank()) {
+                    sumaLinea += Double.parseDouble(cadenaLeida);
+                    numeroLinea++;
+                }
+                cadenaLeida = bufferIn.readLine();
+                System.out.println(sumaLinea);
+     
+            }
+            
+            bufferIn.close();
+        } catch (IOException ex) {
+            System.out.println("Archivo no encontrado.");
+        }
+        media = sumaLinea / numeroLinea;
+        System.out.println("Suma total: " + sumaLinea);
+        System.out.println("Media: " + media);
 				
 	}
 
